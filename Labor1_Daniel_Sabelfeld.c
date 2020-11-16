95bb6b22f0e23b0e218b129fbb0de57c21298f9e
W3asd <73559231+W3asd@users.noreply.github.com>
2020-11-16 02:49:14 +0100

    Update Labor1_Daniel_Sabelfeld.c

--- Labor1_Daniel_Sabelfeld.c
+++ Labor1_Daniel_Sabelfeld.c
@@ -1,6 +1,6 @@
 /******************************************************************
-* Projekt:ProgrammierenInC Dateiname: ??? Beschreibung: ???
-* Autor: Max Mustermann (Matr.-Nr. 911911), Erstellt: Sept. 2011
+* Projekt:ProgrammierenInC Dateiname: Labor1_Daniel_Sabelfeld.c Beschreibung: ???
+* Autor: Daniel Sabelfeld (Matr.-Nr. 937299), Erstellt: Nov. 2020
 ******************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,7 +10,9 @@ int main()
     int a,b;
     float erg;
 
-    printf("Ein erstes Programm...\n");
+    printf("Aufgabe 1\n");
+
+    printf("Das Programm nimmt die beiden int und addiert, subtrahiert, dividiert und multipliziert die eingegebenen Werte\n Das Ergebnis wird in der Variable erg als float gespeichert \n");
     printf("\n");
 
     printf("Bitte die erste (ganze) Zahleingeben:\n");
@@ -30,6 +32,25 @@ int main()
     
     erg = a / b;
     printf("%d/%d=%f\n",a,b,erg); 
-    getchar();
+
+    
+    
+    printf("\nAufgabe 2\n\n");
+    printf(". (Punkt)\n- (Minuszeichen)\n` (Hochkomma)\n& (Kaufmanns-„Und“)\n/ (Slash)\n\\ (Backslash)\n_ (Unterstrich)\n| (Strich)\n( (Klammer auf)\n) (Klammer zu)\n\" (Anführungsstriche)\n` (einfacher Anführungsstrich)\no (kleines o)\n\n");
+    printf("Aufgabe 3\n");
+    printf("      .-'&    '-.\n");
+    printf("     /            \\\n");
+    printf("    :   o    o     ;\n");
+    printf("   (     (_         )\n");
+    printf("    :              ;\n");
+    printf("     \\    __      /\n");
+    printf("      `-._____.-'\n");
+    printf("       / `\"\"\"` \\\n");
+    printf("      /|/\\/\\/\\ _\\\n");
+    printf("     (_|/\\/\\/\\\\__)\n");
+    printf("       |_______|\n");
+    printf("      __)_ |_ (__\n");
+    printf("     (_____|_____)\n");
+
     return 0;
 }
